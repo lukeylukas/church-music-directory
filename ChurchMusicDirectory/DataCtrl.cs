ac1731aebd34953851f84e831998a377f86d6b6f
Luke Lewis <llewis@skytrac.ca>
2022-12-20 15:07:08 -0800

    added worshipElements list; re-architected DataCtrl for use with multiple database tables added serviceRecords fetcher methods

--- ChurchMusicDirectory/DataCtrl.cs
+++ ChurchMusicDirectory/DataCtrl.cs
@@ -14,7 +14,7 @@ namespace ChurchMusicDirectory
     public class DataCtrl
     {
         public DataTable songInfoTable;
-        public DataTable theOtherTable;
+        public DataTable serviceRecordsTable;
         public delegate void DataCtrlResponseHandler(bool success, string message);
         public enum SONG_ATTRIBUTE
         {
@@ -24,6 +24,19 @@ namespace ChurchMusicDirectory
             numPlays,
             tag,
             COUNT
+        }
+        public enum SERVICE_RECORD_ATTRIBUTE
+        {
+            date,
+            serviceNumber,
+            orderInService,
+            elementName,
+            title,
+            musicKey,
+            passage,
+            notes,
+            COUNT
+
         }
         public struct TABLE_COLUMN
         {
@@ -71,49 +84,107 @@ namespace ChurchMusicDirectory
             },
         };
 
+        public List<string> worshipElements = new List<string>
+        {
+            "Song",
+            "Sermon",
+            "Offering",
+            "Prayer",
+            "Pastoral Prayer",
+            "Communion",
+            "Invocation",
+            "Benediction",
+            "Responsive Reading",
+            "Scripture Reading",
+            "Announcements",
+            "Greeting",
+        };
+
         public DataCtrl()
         {
             songInfoTable = new DataTable();
-            theOtherTable = new DataTable();
+            serviceRecordsTable = new DataTable();
         }
 
         public bool GetSongInfo(DataCtrlResponseHandler callback)
         {
-            bool songInfoReceived = false;
-            string statusMessage = "";
+            string songInfoQuery = BuildSongInfoQuery();
+            int expectedNumColumns = (int)SONG_ATTRIBUTE.COUNT;
 
-            string columns = "";
-            for (SONG_ATTRIBUTE attribute = 0; attribute < SONG_ATTRIBUTE.COUNT; attribute++)
+            bool songInfoReceived = GetTableData(out songInfoTable, songInfoQuery, expectedNumColumns, out string statusMessage);
+            if (statusMessage.Length != 0)
             {
-                columns += attribute + ", ";
-            }
-            columns = columns.Substring(0, columns.Length - 2);
-
-            string query = "SELECT " + columns + " FROM songInfo";
-
-            DataTable tempTable = new DataTable();
-
-            if (ServerCommunication.QuerySqlServer(query, out tempTable))
-            {
-                if (tempTable.Columns.Count == (int)SONG_ATTRIBUTE.COUNT)
-                {
-                    songInfoReceived = true;
-                    songInfoTable = tempTable;
-                }
-                else
-                {
-                    statusMessage = "Server sent incorrect data size.";
-                    MessageBox.Show(statusMessage);
-                }
-            }
-            else
-            {
-                statusMessage = "Response not received";
                 MessageBox.Show(statusMessage);
             }
 
             callback(songInfoReceived, statusMessage);
             return songInfoReceived;
+        }
+
+        private string BuildSongInfoQuery()
+        {
+            string columns = "";
+            string connectorString = ", ";
+            for (SONG_ATTRIBUTE attribute = 0; attribute < SONG_ATTRIBUTE.COUNT; attribute++)
+            {
+                columns += attribute + connectorString;
+            }
+            columns = columns.Substring(0, columns.Length - connectorString.Length);
+
+            return "SELECT " + columns + " FROM songInfo";
+        }
+
+        public bool GetTableData(out DataTable destTable, string query, int expectedNumColumns, out string message)
+        {
+            bool receivedCorrectTable = false;
+            message = "";
+            destTable = new DataTable();
+
+            if (ServerCommunication.QuerySqlServer(query, out DataTable tempTable))
+            {
+                if (tempTable.Columns.Count == expectedNumColumns)
+                {
+                    receivedCorrectTable = true;
+                    destTable = tempTable;
+                }
+                else
+                {
+                    message = "Server sent incorrect data size.";
+                }
+            }
+            else
+            {
+                message = "Response not received.";
+            }
+
+            return receivedCorrectTable;
+        }
+
+        public bool GetServiceRecords(DataCtrlResponseHandler callback)
+        {
+            string serviceRecordsQuery = BuildServiceRecordsQuery();
+            int expectedNumColumns = (int)SERVICE_RECORD_ATTRIBUTE.COUNT;
+
+            bool serviceRecordsReceived = GetTableData(out serviceRecordsTable, serviceRecordsQuery, expectedNumColumns, out string statusMessage);
+            if (statusMessage.Length != 0)
+            {
+                MessageBox.Show(statusMessage);
+            }
+
+            callback(serviceRecordsReceived, statusMessage);
+            return serviceRecordsReceived;
+        }
+        private string BuildServiceRecordsQuery()
+        {
+            string columns = "";
+            string connectorString = ", ";
+            for (SERVICE_RECORD_ATTRIBUTE attribute = 0; attribute < SERVICE_RECORD_ATTRIBUTE.COUNT; attribute++)
+            {
+                columns += attribute + connectorString;
+            }
+            columns = columns.Substring(0, columns.Length - connectorString.Length);
+
+            return "SELECT " + columns + " FROM serviceRecords";
         }
     }
 }
